07a9ccfd884a4ca8b8312b60299ad2098c951a7b
Justin Coole <jcoole@sprockitconsulting.com>
2023-06-09 14:57:37 -0700

    Add VsoParam annotations

--- o11nplugin-vrslcm-core/src/main/java/com/sprockitconsulting/vrslcm/plugin/services/LifecycleOperationsService.java
+++ o11nplugin-vrslcm-core/src/main/java/com/sprockitconsulting/vrslcm/plugin/services/LifecycleOperationsService.java
@@ -21,6 +21,7 @@ import com.sprockitconsulting.vrslcm.plugin.scriptable.Request;
 import com.sprockitconsulting.vrslcm.plugin.scriptable.VirtualCenter;
 import com.vmware.o11n.plugin.sdk.annotation.VsoMethod;
 import com.vmware.o11n.plugin.sdk.annotation.VsoObject;
+import com.vmware.o11n.plugin.sdk.annotation.VsoParam;
 
 /**
  * This service handles Lifecycle Operations section specific content.
@@ -49,12 +50,19 @@ public class LifecycleOperationsService extends AbstractService {
 	}
 
 	@VsoMethod(description = "Creates a new Datacenter with the given name and location.")
-	public Datacenter createDatacenter(String name, String location) throws JsonProcessingException {
+	public Datacenter createDatacenter(
+			@VsoParam(description = "Name of the Datacenter to create.")String name, 
+			@VsoParam(description = "Location of the Datacenter.")String location
+			) throws JsonProcessingException {
 		return datacenterService.create(connection, new Datacenter(name, location));
 	}
 	
 	@VsoMethod(description = "Updates an existing Datacenter's name and/or location.")
-	public Datacenter updateDatacenter(Datacenter dc, String name, String location) {
+	public Datacenter updateDatacenter(
+			@VsoParam(description = "The Datacenter to update.")Datacenter dc, 
+			@VsoParam(description = "Updated Datacenter name.")String name, 
+			@VsoParam(description = "Updated Datacenter location.")String location
+			) {
 		return datacenterService.update(connection, dc, new Datacenter(name,location) );
 	}
 	
@@ -64,12 +72,16 @@ public class LifecycleOperationsService extends AbstractService {
 	}
 	
 	@VsoMethod(description = "Retrieves a Datacenter by name or resource ID on this server.")
-	public Datacenter getDatacenterByValue(String nameOrId) {
+	public Datacenter getDatacenterByValue(
+			@VsoParam(description = "Name or ID of the Datacenter to retrieve.")String nameOrId
+			) {
 		return datacenterService.getByValue(connection, nameOrId);
 	}
 	
 	@VsoMethod(description = "Deletes a Datacenter on the server. Returns a request to monitor for completion.")
-	public void deleteDatacenter(Datacenter dc) {
+	public void deleteDatacenter(
+			@VsoParam(description = "The Datacenter to delete.")Datacenter dc
+			) {
 		datacenterService.delete(connection, dc);
 
 	}
@@ -80,7 +92,9 @@ public class LifecycleOperationsService extends AbstractService {
 	}
 	
 	@VsoMethod(description = "Retrieves an Environment by name or resource ID on this server.")
-	public Environment getEnvironmentByValue(String nameOrId) {
+	public Environment getEnvironmentByValue(
+			@VsoParam(description = "Name or ID of the Environment to retrieve.")String nameOrId
+			) {
 		return environmentService.getByValue(connection, nameOrId);
 	}
 	
@@ -90,37 +104,55 @@ public class LifecycleOperationsService extends AbstractService {
 	}
 	
 	@VsoMethod(description = "Retrieves a Request by resource ID on this server.")
-	public Request getRequestById(String requestId) {
+	public Request getRequestById(
+			@VsoParam(description = "ID of the Request.")String requestId
+			) {
 		return requestService.getByValue(connection, requestId);
 	}
 	
 	@VsoMethod(description = "Retrieves a vCenter in a Datacenter by name.")
-	public VirtualCenter getVirtualCenter(Datacenter dc, String name) {
+	public VirtualCenter getVirtualCenter(
+			@VsoParam(description = "The Datacenter to search.")Datacenter dc, 
+			@VsoParam(description = "Name of the vCenter.")String name
+			) {
 		return virtualCenterService.getByName(dc, name);
 	}
 	
 	@VsoMethod(description = "Retrieves all vCenter connections on the server.")
-	public List<VirtualCenter> getAllVirtualCenters(Connection connection) {
+	public List<VirtualCenter> getAllVirtualCenters() {
 		return virtualCenterService.getAll(connection);
 	}
 	
 	@VsoMethod(description = "Request to update a vCenter's information and properties.")
-	public Request updateVirtualCenter(Datacenter dc, VirtualCenter original, VirtualCenter updated) {
+	public Request updateVirtualCenter(
+			@VsoParam(description = "The Datacenter containing the vCenter connection.")Datacenter dc, 
+			@VsoParam(description = "The vCenter to update.")VirtualCenter original, 
+			@VsoParam(description = "The vCenter object with updated values.")VirtualCenter updated
+			) {
 		return virtualCenterService.update(dc, original, updated);
 	}
 	
 	@VsoMethod(description = "Request creation of a new vCenter Connection in the Datacenter.")
-	public Request createVirtualCenter(Datacenter dc, VirtualCenter vc) {
+	public Request createVirtualCenter(
+			@VsoParam(description = "The Datacenter to create the vCenter in.")Datacenter dc, 
+			@VsoParam(description = "The vCenter object to create.")VirtualCenter vc
+			) {
 		return virtualCenterService.create(dc, vc);
 	}
 	
 	@VsoMethod(description = "Request deleting a vCenter from the Datacenter. If any Environments are associated with it, the Request will fail!")
-	public Request deleteVirtualCenter(Datacenter dc, String name) {
+	public Request deleteVirtualCenter(
+			@VsoParam(description = "The Datacenter containing the vCenter connection to delete.")Datacenter dc, 
+			@VsoParam(description = "Name of the vCenter connection to delete.")String name
+			) {
 		return virtualCenterService.delete(dc, name);
 	}
 	
 	@VsoMethod(description = "Request to perform a data collection on a vCenter.")
-	public Request syncVirtualCenter(Datacenter dc, String name) {
+	public Request syncVirtualCenter(
+			@VsoParam(description = "The Datacenter containing the vCenter connection to sync.")Datacenter dc, 
+			@VsoParam(description = "Name of the vCenter to sync.")String name
+			) {
 		return virtualCenterService.sync(dc, name);
 	}
 	
